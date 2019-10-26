0739693ff5fada4fbd33cfaded1513ff2ccf3f14
baredh821 <baredh821@gmail.com>
2019-10-26 13:14:16 -0500

    Working APis

--- project01/script.js
+++ project01/script.js
@@ -1,15 +1,13 @@
-var searchbutton = $("#searchbar").val().trim()
+// var searchbutton = $("#searchbar").val().trim()
 var key = "bf3bbf0ea22e7e35ceaa37777ebf0b82"
 var proxy = "https://chriscastle.com/proxy/index.php?:proxy:";
 
-//call local storage here to retrieve data
-
 https://www.googleapis.com/civicinfo/v2/elections
-function electionInfo(searchValue) {
-    searchValue = encodeURI(searchValue);
+function electionInfo(searchValue, searchValue1) {
+    console.log(searchValue)
     var searchURL = "https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyCfJ43LRmmtdLASppmxY0NB5aIfzi5knmY&address=" + searchValue
     console.log(searchURL)
-    var searchURL1 = "https://api.propublica.org/congress/v1/116/nominees/state/VA.json";
+    var searchURL1 = "https://api.propublica.org/congress/v1/116/nominees/state/" + searchValue1 + ".json";
     console.log(searchURL1)
     $.ajax({
 
@@ -31,8 +29,12 @@ function electionInfo(searchValue) {
 
 $("#searchbutton").on("click", function (event) {
     event.preventDefault()
-    var value = $("#searchbar").val().trim();
-    console.log(value)
-    electionInfo(value)
+    var value = $("#houseNo").val().trim();
+    var value1 = $("#StreetName").val().trim();
+    var value2 = $("#city").val().trim();
+    var value3 = $("#state").val().trim();
+    var value4 = $("#zipcode").val().trim();
+    var allValues = (value + "%20" + value1 + "%20" + value2 + "%20" + value3 + "%20" + value4);
+    electionInfo(allValues, value3);
 })
 
